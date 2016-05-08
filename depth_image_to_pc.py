1dafbdbeb9b755526d58f80023b8de8387370d5a
CJ Geering <cgeering1203@gmail.com>
2016-05-08 13:12:19 -0700

    Updated depth_image_to_pc to implement boxing.

--- depth_image_to_pc.py
+++ depth_image_to_pc.py
@@ -18,34 +18,47 @@ def dimg_to_pc(dimg, K, trans, rot):
 			pc.append(x_base)
 	return pc
 
+def boxer(pc, bounds):
+    potentials = []
+    for i in range(3):
+        print(i)
+        pl = np.where(pc[:,i] >= bounds[0][0])[0]
+        ph = np.where(pc[:,i] <= bounds[0][1])[0]
+        p = np.intersect1d(pl, ph)
+        potentials.append(p)
+    p = np.intersect1d(potentials[0], potentials[1])
+    p = np.intersect1d(p, potentials[2])
+    return pc[p]
+
 def merge_dimg_to_pcs(dimg1, dimg2, trans1, rot1, trans2, rot2):
 	h1, w1 = dimg1.shape
 	h2, w2 = dimg2.shape
 	K1 = np.array([[525,0,w1/2],[0,525,h1/2],[0,0,1]], dtype=np.uint16)
-	K2 = np.array([[525,0,w2/2],[0,525,h2/2],[0,0,1]], dtype=np.uint16) # K1 should equal K2 when the depthm images are of the same size
+	K2 = np.array([[525,0,w2/2],[0,525,h2/2],[0,0,1]], dtype=np.uint16) # K1 should equal K2 when the depth images are of the same size
 	pc1 = dimg_to_pc(dimg1, K1, trans1, rot1)
 	pc2 = dimg_to_pc(dimg2, K2, trans2, rot2)
-	return pc1 + pc2
+	return np.array(pc1 + pc2)
 
-def tester():
-	""" The steps for collecting depth image and then converting to a merged
-	point cloud in Baxter's base frame:
+"""The steps for collecting depth image and then converting to a merged
+point cloud in Baxter's base frame:
 
-	1. Collect an averaged depth image from each Kinect. Call these dimg1 and dimg2
-	for Kinect1 and Kinect2 respectively. (This should be done in a separate method).
+1. Collect an averaged depth image from each Kinect. Call these dimg1 and dimg2
+for Kinect1 and Kinect2 respectively. (This should be done in a separate method).
 
+2. Compute the tfs for each Kinect to the base, i.e. FROM KinectX TO base -->
+tf.lookupTransform('base', /KinectX_link, rospy.time(0)). Store these tfs as
+transX, rotX for the respective KinectX.
 
-	2. Compute the tfs for each Kinect to the base, i.e. FROM KinectX TO base -->
-	tf.lookupTransform('base', /KinectX_link, rospy.time(0)). Store these tfs as
-	transX, rotX for the respective KinectX.
+3. Call merge_dimg_to_pcs(dimg1, dimg2, trans1, rot1, trans2, rot2) to get 
+a merged pointcloud in Baxter's base frame. The merged pointcloud
+will be an nX3 numpy array.
 
-	3. Call merge_dimg_to_pcs(dimg1, dimg2, trans1, rot1, trans2, rot2) to get 
-	a merged pointcloud in Baxter's base frame. The merged pointcloud
-	will be a Python List of (1x3) numpy arrays.
+4. (Optional) Develop a method to visualize/publish the pointcloud to double 
+check correctness.
 
-	4. (Optional) Develop a method to visualize/publish the pointcloud to double 
-	check correctness.
+5. Run the merged pointcloud through the bounding box by calling boxer() with 
+the desired arguments.
 
-	5. Run the merged pointcloud through the bounding box.
+6. (Optional) Visualize this new bounded pointcloud.
 
-	6. Send this boxed pointcoud to featurizer.""""
+7. Send this boxed pointcoud to sampler."""
